db92e5f32c4aba666336e235ebb398a3e42e254b
MAGICPEA <MAGICPEA@UW.EDU>
2021-02-12 16:12:54 -0800

    Added js

--- js/script.js
+++ js/script.js
@@ -101,3 +101,16 @@
 });
 
 })(jQuery);
+
+// Toggle hide/show FAQ
+
+$(document).ready(function() {
+	$('[data-toggle="collapse"]').click(function() {
+		$(this).toggleClass( "active" );
+		if ($(this).hasClass("active")) {
+			$(this).text("Hide");
+		} else {
+			$(this).text("Show");
+		}
+	});
+	});
